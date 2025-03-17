8211cded2f3c095173bc3f89fe3691f4b7b78e40
Tomas Bankauskas <tbdizainas@gmail.com>
2025-03-17 13:47:41 +0200

    updated _app.js (#128)
    * updated _app.js
    * removed comment

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,18 @@
 import { generateGlobalCssVariables } from '@/utils/theme-style-utils';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import '../css/main.css';
 
 export default function MyApp({ Component, pageProps }) {
     const { global, ...page } = pageProps;
     const { theme } = global || {};
+    const [isMounted, setIsMounted] = useState(false);
 
     const cssVars = generateGlobalCssVariables(theme);
 
     useEffect(() => {
+        setIsMounted(true);
         document.body.setAttribute('data-theme', page.colors || 'colors-a');
-    });
+    }, [page.colors]);
 
     return (
         <>
@@ -19,7 +21,7 @@ export default function MyApp({ Component, pageProps }) {
                     ${cssVars}
                 }
             `}</style>
-            <Component {...pageProps} />
+            {isMounted ? <Component {...pageProps} /> : null}
         </>
     );
 }
